46360c84e918bdb08cd5386719c1aa49f7685062
Lane Miller <191768901+lane-miller@users.noreply.github.com>
2026-07-26 11:33:51 -0400

    Begin 3rd poc - conv generated kernels with speech

--- 01_poc/util.py
+++ 01_poc/util.py
@@ -7,6 +7,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import cumulative_trapezoid as cti
 
+# Chirp constraint constants
+MIN_RATIO = 1.414  # sqrt(2), i.e. 1/2 octave minimum sweep
+MAX_BW = 2000
+MIN_BW = 200
+MAX_FREQ = 7000
+MIN_FREQ = 50
+MAX_DUR_MS = 100
+MIN_DUR_MS = 10
+MAX_DEGREE = 3
+MIN_DEGREE = 0.5
+
 # pchirp - single term polynomial chirplet
 def pchirp(freq1, freq2, duration_ms, degree):
     """
@@ -23,3 +34,33 @@ def pchirp(freq1, freq2, duration_ms, degree):
     phi_i = 2 * np.pi * cti(f_i, t, initial=0)
 
     return np.sin(phi_i) * np.hanning(len(phi_i))
+
+# Get bw and fc within constraints
+def pick_bw_fc():
+    """
+    Selects bw and fc that automatically respect imposed constraints
+    """
+    # Define viable bw selection range based on constraints
+    bw_lo = max(MIN_BW, (MIN_FREQ * (MIN_RATIO - 1)))
+    bw_hi = min(MAX_BW, (MAX_FREQ - MIN_FREQ))
+
+    # Select bw
+    bw = pick_val(bw_lo, bw_hi)
+    
+    # Define viable fc selection range
+    fc_lo = MIN_FREQ + bw / 2
+    fc_hi = min((MAX_FREQ - bw / 2), (bw / 2 * (MIN_RATIO + 1) / (MIN_RATIO - 1)))
+
+    # Select fc
+    fc_log = pick_val(np.log(fc_lo), np.log(fc_hi))
+    fc = np.exp(fc_log)
+    
+    return bw, fc
+
+# Pick val from uniform distribution
+def pick_val(lower_bnd, upper_bnd):
+    return np.random.uniform(lower_bnd, upper_bnd)
+
+# Get lower and upper band lims from fc and and bw
+def bw_fc_to_f1f2(bw, fc):
+    return fc - bw / 2, fc + bw / 2
